73421015d42246fed4270bf45eff3fb3de3969a2
ediscokes <edwardkoranteng2002@gmail.com>
2024-05-17 16:39:14 +0100

    Task one

---
+++ arrayManipulation.js
@@ -0,0 +1,15 @@
+function processArray (numbers){
+    return numbers.map(num => {
+        if (num%2 ===0){
+            return num*num;
+        } else{
+
+            return num*3;
+        }
+    });
+
+}
+
+const numbers = [1,2,3,4,5,6,7,8,9,10,12]
+const result =processArray(numbers);
+console.log(result);
