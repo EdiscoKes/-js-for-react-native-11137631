72c4fb3b65836a745bb1843422e994061ebceb21
ediscokes <edwardkoranteng2002@gmail.com>
2024-05-18 16:44:21 +0100

    "ask Two ,Added example usage for processArray function and introduced new function formatArrayStrings with its own example usage."

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -1,3 +1,4 @@
+//Task 1
 function processArray (numbers){
     return numbers.map(num => {
         if (num%2 ===0){
@@ -9,7 +10,31 @@ function processArray (numbers){
     });
 
 }
+//Example usage
 
-const numbers = [1,2,3,4,5,6,7,8,9,10,12]
-const result =processArray(numbers);
-console.log(result);
+const numbers = [1,2,3,4,5]
+ const processedNumbers = processArray(numbers);
+ console.log(processedNumbers);
+
+
+
+//Task 2
+
+function formatArrayStrings(strings,processedNumbers){
+    return strings.map((string,index)=>{
+        if (processedNumbers[index]%2 === 0){
+            return string.toUpperCase();
+        } else{
+            return string.toLowerCase();
+        }
+    });
+
+
+
+}
+
+//example usage
+const strings =["My","Mom","is","the","best"];
+const formattedArrayStrings = formatArrayStrings(strings,processedNumbers);
+console.log(formattedArrayStrings);
+
